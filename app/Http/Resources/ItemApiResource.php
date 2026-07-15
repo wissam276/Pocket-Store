751517dd451ed473edda84173697d078f02b362c
Wissam Mdgmesh <samowisam4.oal@gmail.com>
2026-07-15 18:28:11 +0300

    small edit on itemApiResoure

--- app/Http/Resources/ItemApiResource.php
+++ app/Http/Resources/ItemApiResource.php
@@ -10,16 +10,30 @@ class ItemApiResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'                => $this->id,
-            'name'              => $this->name,
-            'slug'              => $this->slug,
-            'price'             => $this->price,
-            'priceAfterDiscount'=> $this->priceAfterDiscount,
-            'description'       => $this->description,
-            'company'           => $this->company,
-            'availability'      => (bool) $this->availability,
-            'item_image'        => asset('storage/' . $this->item_image),
-            'details_images'    => array_map(fn($path) => asset('storage/' . $path), json_decode($this->details_image, true) ?? []),
+            'id'                 => $this->id,
+            'name'               => $this->name,
+            'slug'               => $this->slug,
+
+
+            'category_id'        => $this->category_id,
+            'quantity'           => $this->quantity,
+            'sales_count'        => $this->sales_count,
+            'DiscountPercentage' => $this->DiscountPercentage,
+            'short_description' => $this->short_description,
+
+
+            'price'              => $this->price,
+            'priceAfterDiscount' => $this->priceAfterDiscount,
+            'description'        => $this->description,
+            'company'            => $this->company,
+            'availability'       => (bool) $this->availability,
+
+            'item_image' => asset('storage/' . $this->item_image),
+
+            'details_images' => array_map(
+                fn($path) => asset('storage/' . $path),
+                json_decode($this->details_image, true) ?? []
+            ),
 
             'stats' => [
                 'average_rating' => $this->average_rating,
